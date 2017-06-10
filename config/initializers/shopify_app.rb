874afe184bb9ee19a58193bf426532e3d39fc4c5
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-10 10:10:54 +0700

    direct_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "b1bdbfb3b87c7818acc43d730c0c2e9f"
   config.secret = "67815a8a83814cb66fc1c1dbc75962a2"
+  config.redirect_uri = "https://testshopifyezgamio.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
