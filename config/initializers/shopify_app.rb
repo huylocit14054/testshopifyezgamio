22fde22fb3a1ee6c2ee65abfabb9ddaac50835c7
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-10 12:36:03 +0700

    fix uri_bug3

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "b1bdbfb3b87c7818acc43d730c0c2e9f"
   config.secret = "67815a8a83814cb66fc1c1dbc75962a2"
-  config.redirect_uri="http://testshopifyezgamio.herokuapp.com/auth/shopify/callback/"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
