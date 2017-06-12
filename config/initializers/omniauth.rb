9167c8492f201a9fa675837acdcf218f3c7d43be
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 13:00:07 +0700

    fix redirect_uri in omniauth 11 time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    scope: ShopifyApp.configuration.scope
-    redirect_uri ="https://testshopifyezgamio.herokuapp.com/auth/shopify/callback"
+    scope: ShopifyApp.configuration.scopes
 end
