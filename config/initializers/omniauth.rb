1b326a50d91e6aec4c72130c23079901df9c9940
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-12 13:02:40 +0700

    fix redirect_uri in omniauth 11 time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    scope: ShopifyApp.configuration.scopes
+    scope: ShopifyApp.configuration.scope
+
 end
