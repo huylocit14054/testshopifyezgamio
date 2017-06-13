7988593a46c9048871080ff49cdd70ff1079023e
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-13 12:57:39 +0700

    test API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @customers = ShopifyAPI::Customer.find(:all)
+    @orders = ShopifyAPI::Order.find(:all)
   end
 end
