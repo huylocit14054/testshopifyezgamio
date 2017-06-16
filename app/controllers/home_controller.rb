5b01a1800ee7a35b355c4647b4b286a67b1b7045
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-16 18:47:03 +0700

    fix controller add_to_theme 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ShopifyApp::AuthenticatedController
     @themes = ShopifyAPI::Theme.find(:all)
   end
   def add_to_theme(id)
-  		@asset_theme = ShopifyAPI::Assert.find('layout/theme.liquid', :params => {:theme_id => id})
+  		@asset_theme = ShopifyAPI::Assert.find('Layout/theme.liquid', :params => {:theme_id => id})
   		@asset_theme.value.sub!("<body class='template-{{ template | split: '.' | first }}'>",
   								"<body class='template-{{ template | split: '.' | first }}'<p>#{id}</p>")
   		asset.save
-  		render index
+  		render 'index'
   end 
 end
