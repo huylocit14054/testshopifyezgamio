4b7e2d762f6b8c4dd9cef6c6c75d0ffe49c3b72d
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-16 20:10:45 +0700

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => 'home#index'
 
 	 match "home/:id/add_to_theme" => "home#index" , :via => :post
+	 
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
