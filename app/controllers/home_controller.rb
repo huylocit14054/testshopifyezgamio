80c3f67a81ab11add09b02697eee172cc0446abc
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-16 16:27:32 +0700

    save new

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @theme = ShopifyAPI::Theme.find(:all)
+    @themes = ShopifyAPI::Theme.find(:all)
   end
 end
