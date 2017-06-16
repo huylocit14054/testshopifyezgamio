edc6f237957cd3fec33ff301bba650350676b9d6
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-16 18:11:28 +0700

    fix route 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
 
-  resources :home
+  resources :home do
+  	asset do
+  		get :add_to_theme
+  	end
+  end
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
