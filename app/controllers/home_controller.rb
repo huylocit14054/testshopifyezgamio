d345bbd2ece4788c6bf6d1ac9cd428d35448b976
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-16 17:32:36 +0700

    test add id to body of theme.liquid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @themes = ShopifyAPI::Theme.find(:all)
   end
+  def add_to_theme(id)
+  		@asset_theme = ShopifyAPI::Assert.find('layout/theme.liquid', :params => {:theme_id => id})
+  		@asset_theme.value.sub!("<body>","<body><p>#{id}</p>")
+  		asset.save
+  		redirect_to "https://test-store-ezgamio.myshopify.com/admin/themes/#{id}?key=layout/theme.liquid"
+  end 
 end
