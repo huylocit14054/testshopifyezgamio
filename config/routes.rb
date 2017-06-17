d43a816b029bcf795a386cf32fafb38f692c6492
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-17 10:18:03 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-
-  resources :home do
-  	member do
-  		match 'home/:id/add_to_theme' => 'home#add_to_theme', :via => [:post] , as: :home_add_to_theme
-  	end
-  end
+  match 'home/:id/add_to_theme' => 'home#add_to_theme', :via => [:post] , as: :home_add_to_theme
+  
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
