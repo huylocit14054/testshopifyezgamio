b7be859e0a8173c8eb46372ab2d87f5b0a4bc3de
Le Dinh Huy Loc <sasuke1996vn@gmail.com>
2017-06-17 10:04:24 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :home do
   	member do
-  		match 'home/:id/add_to_theme' => 'home#add_to_theme', as: :home_add_to_theme
+  		match 'home/:id/add_to_theme' => 'home#add_to_theme', :via => [:post] , as: :home_add_to_theme
   	end
   end
   mount ShopifyApp::Engine, at: '/'
